ec74de1972b48dd6d5e8ab6b8b366562f8ea24f3
Tatsiana Maksimuk <okeanoka@gmail.com>
2023-03-01 23:36:03 -0800

    UserRepository added

---
+++ src/main/java/com/gigmanager/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.gigmanager.repositories;
+import com.gigmanager.models.ApiUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<ApiUser, Long> {
+    //custom method (query) to find user by username:
+    Optional<ApiUser> findByUsername(String username);
+}
