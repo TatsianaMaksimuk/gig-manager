84ae70e3bcb1425cfedc29a04ffc2a3cab3baba3
Tatsiana Maksimuk <okeanoka@gmail.com>
2023-03-17 21:57:35 -0700

    JWTAuthenticationFilter updated to return generated token as a JSON

--- src/main/java/com/gigmanager/filter/JWTAuthenticationFilter.java
+++ src/main/java/com/gigmanager/filter/JWTAuthenticationFilter.java
@@ -51,5 +51,10 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
                 .withExpiresAt(new Date(System.currentTimeMillis() + AuthenticationConfigConstants.EXPIRATION_TIME))
                 .sign(Algorithm.HMAC512(AuthenticationConfigConstants.SECRET.getBytes()));
         response.addHeader(AuthenticationConfigConstants.HEADER_STRING, AuthenticationConfigConstants.TOKEN_PREFIX + token);
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        response.getWriter().write(
+                "{\"" + AuthenticationConfigConstants.HEADER_STRING + "\":\"" + AuthenticationConfigConstants.TOKEN_PREFIX + token + "\"}"
+        );
     }
 }
