93a2cea1da93b36206b30a36b49e8c7d22d10336
Tatsiana Maksimuk <okeanoka@gmail.com>
2023-03-28 00:19:15 -0700

    Changed path for login link, added create, read and update methods to customer

--- src/main/java/com/gigmanager/controllers/CustomersController.java
+++ src/main/java/com/gigmanager/controllers/CustomersController.java
@@ -1,14 +1,16 @@
 package com.gigmanager.controllers;
 
+import com.gigmanager.models.ApiUser;
 import com.gigmanager.models.Customer;
+import com.gigmanager.models.request.CustomerUpsertRequest;
 import com.gigmanager.repositories.CustomerRepository;
+import com.gigmanager.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import javax.persistence.Entity;
 import javax.servlet.http.HttpServletRequest;
@@ -22,15 +24,54 @@ public class CustomersController {
     //CRUD for customers
 
     private final CustomerRepository customerRepository;
+    private final UserRepository userRepository;
 
-    @GetMapping
+    @GetMapping("/")
     public ResponseEntity<?> getAllCustomers(HttpServletRequest request){
         String username = request.getUserPrincipal().getName();
         List<Customer> customers = customerRepository.findAllByApiUser_username(username);
-        return ResponseEntity.ok(customers);
+        return new ResponseEntity<>(customers, HttpStatus.OK);
     }
 
     //get customer by customer id
+    // found customer should belong to current user
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getCustomerById (@PathVariable Long id, HttpServletRequest request){
+        String username = request.getUserPrincipal().getName();
+        Customer requestedCustomer = customerRepository.getById(id);
+        if (!requestedCustomer.getApiUser().getUsername().equals(username)){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(requestedCustomer, HttpStatus.OK);
+    }
+
+    //create customer
+    @PostMapping("/")
+    public ResponseEntity<?> createCustomer(@RequestBody CustomerUpsertRequest customerUpsertRequest, HttpServletRequest request) {
+        String username = request.getUserPrincipal().getName();
+        ApiUser user = userRepository.findByUsername(username).orElseThrow();
+        Customer newCustomer = new Customer();
+        newCustomer.setApiUser(user);
+        newCustomer.setName(customerUpsertRequest.getName());
+        newCustomer.setEmail(customerUpsertRequest.getEmail());
+        customerRepository.save(newCustomer);
+        return new ResponseEntity<>(newCustomer, HttpStatus.CREATED);
+
+    }
+
+    //update customer
+   @PostMapping("/{id}")
+    public ResponseEntity<?> updateCustomer(@PathVariable Long id, @RequestBody CustomerUpsertRequest customerUpsertRequest, HttpServletRequest request) {
+       String username = request.getUserPrincipal().getName();
+       Customer requestedCustomer = customerRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+       if (!requestedCustomer.getApiUser().getUsername().equals(username)){
+           return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+       }
+       requestedCustomer.setName(customerUpsertRequest.getName());
+       requestedCustomer.setEmail(customerUpsertRequest.getEmail());
+       customerRepository.save(requestedCustomer);
+       return new ResponseEntity<>(requestedCustomer, HttpStatus.OK);
+   }
 
 
 
