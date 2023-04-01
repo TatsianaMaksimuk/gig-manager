c2bcaa3981a210350fb7c9d55411087a069bed1f
Tatsiana Maksimuk <okeanoka@gmail.com>
2023-04-01 15:40:56 -0700

    fixed query paths

--- src/main/java/com/gigmanager/repositories/JobRepository.java
+++ src/main/java/com/gigmanager/repositories/JobRepository.java
@@ -1,11 +1,13 @@
 package com.gigmanager.repositories;
 
 import com.gigmanager.models.Job;
+import com.gigmanager.models.JobStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface JobRepository extends JpaRepository<Job, Long> {
     //query all jobs by customer id
-    List<Job> filterAllByCustomer_id(Long id);
+    List<Job> findAllByCustomer_id(Long id);
+    List<Job> findAllByStatus(JobStatus status);
 }
