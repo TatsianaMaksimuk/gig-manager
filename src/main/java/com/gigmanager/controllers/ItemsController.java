e3d01ef3dbd931772133dae517dc653f384de344
Tatsiana Maksimuk <okeanoka@gmail.com>
2023-05-09 21:05:13 -0700

    CRUD for Items added

--- src/main/java/com/gigmanager/controllers/ItemsController.java
+++ src/main/java/com/gigmanager/controllers/ItemsController.java
@@ -1,8 +1,12 @@
 package com.gigmanager.controllers;
 
 import com.gigmanager.models.Item;
+import com.gigmanager.models.Job;
 import com.gigmanager.models.enums.ItemStatus;
+import com.gigmanager.models.request.ItemUpsertRequest;
+import com.gigmanager.models.request.JobUpsertRequest;
 import com.gigmanager.repositories.ItemRepository;
+import com.gigmanager.repositories.JobRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,10 +21,11 @@ import java.util.List;
 @RequiredArgsConstructor
 public class ItemsController {
     private final ItemRepository itemRepository;
+    private final JobRepository jobRepository;
 
     //get all items
     @GetMapping("/items")
-    public ResponseEntity<?> getAllItems(HttpServletRequest request){
+    public ResponseEntity<?> getAllItems(HttpServletRequest request) {
         String username = request.getUserPrincipal().getName();
         List<Item> items = itemRepository.findAllByJob_Customer_ApiUser_username(username);
         return new ResponseEntity<>(items, HttpStatus.OK);
@@ -28,38 +33,77 @@ public class ItemsController {
 
     //get item by id
     @GetMapping("items/{id}")
-    public  ResponseEntity<?> getItemById(@PathVariable Long id, HttpServletRequest request){
+    public ResponseEntity<?> getItemById(@PathVariable Long id, HttpServletRequest request) {
         String username = request.getUserPrincipal().getName();
         Item requestedItem = itemRepository.findById(id).orElse(null);
-        if (requestedItem==null||!requestedItem.getJob().getCustomer().getApiUser().getUsername().equals(username)){
+        if (requestedItem == null || !requestedItem.getJob().getCustomer().getApiUser().getUsername().equals(username)) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
-        return new ResponseEntity<>(requestedItem,HttpStatus.OK);
+        return new ResponseEntity<>(requestedItem, HttpStatus.OK);
     }
 
     //get all items by job id
- @GetMapping("jobs/{id}/items")
-    public ResponseEntity<?> getAllItemsByJobId(@PathVariable long id, HttpServletRequest request){
-     String username = request.getUserPrincipal().getName();
-     List<Item> items = itemRepository.findAllByJob_Customer_ApiUser_usernameAndJob_id(username,id);
-     return new ResponseEntity<>(items, HttpStatus.OK);
- }
+    @GetMapping("jobs/{id}/items")
+    public ResponseEntity<?> getAllItemsByJobId(@PathVariable long id, HttpServletRequest request) {
+        String username = request.getUserPrincipal().getName();
+        List<Item> items = itemRepository.findAllByJob_Customer_ApiUser_usernameAndJob_id(username, id);
+        return new ResponseEntity<>(items, HttpStatus.OK);
+    }
 
 
     //get items by status and job id
 
-@GetMapping("jobs/{id}/items/filter")
-    public ResponseEntity<?> getItemsByJobAndStatusFilter(@PathVariable long id, @RequestParam ItemStatus status, HttpServletRequest request){
-    String username = request.getUserPrincipal().getName();
-    List<Item> items = itemRepository.findAllByJob_Customer_ApiUser_usernameAndJob_idAndStatus(username, id, status);
-    return new ResponseEntity<>(items, HttpStatus.OK);
-}
+    @GetMapping("jobs/{id}/items/filter")
+    public ResponseEntity<?> getItemsByJobAndStatusFilter(@PathVariable long id, @RequestParam ItemStatus status, HttpServletRequest request) {
+        String username = request.getUserPrincipal().getName();
+        List<Item> items = itemRepository.findAllByJob_Customer_ApiUser_usernameAndJob_idAndStatus(username, id, status);
+        return new ResponseEntity<>(items, HttpStatus.OK);
+    }
+
+
     //create
+    @PostMapping("jobs/{id}")
+    public ResponseEntity<?> createItem(@RequestBody ItemUpsertRequest itemUpsertRequest, Long id, HttpServletRequest request) {
+        String username = request.getUserPrincipal().getName();
+        Job itemJob = jobRepository.getById(id);
+        if (!itemJob.getCustomer().getApiUser().getUsername().equals(username)) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        Item newItem = new Item();
+        newItem.setJob(itemJob);
+        newItem.setName(itemUpsertRequest.getName());
+        newItem.setDescription(itemUpsertRequest.getDescription());
+        newItem.setStatus(ItemStatus.TO_DO);
+        newItem.setStage(itemUpsertRequest.getStage());
+        return new ResponseEntity<>(newItem, HttpStatus.CREATED);
+    }
 
     //update
+    @PostMapping("jobs/{jobId}/items/{itemId}")
+    public ResponseEntity<?> updateItem(@RequestBody ItemUpsertRequest itemUpsertRequest, Long jobId, long itemId, HttpServletRequest request) {
+        String username = request.getUserPrincipal().getName();
+        Item item = itemRepository.getById(itemId);
+        if (!item.getJob().getId().equals(jobId)
+                || !item.getJob().getCustomer().getApiUser().getUsername().equals(username)) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        item.setName(itemUpsertRequest.getName());
+        item.setDescription(itemUpsertRequest.getDescription());
+        item.setStatus(itemUpsertRequest.getStatus());
+        item.setStage(itemUpsertRequest.getStage());
+        item.setFinished(itemUpsertRequest.isFinished());
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 
     //delete
-
-
+    @DeleteMapping("item/{id}")
+    public ResponseEntity<?> deleteItem(@PathVariable Long id, HttpServletRequest request) {
+        String username = request.getUserPrincipal().getName();
+        Item requestedItem = itemRepository.findById(id).orElse(null);
+        if (requestedItem != null && requestedItem.getJob().getCustomer().getApiUser().getUsername().equals(username)) {
+            itemRepository.deleteById(id);
+        }
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 
 }
